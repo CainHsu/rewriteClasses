f964a9f4905110135bc958920c9873fec528e6e8
Cain <xu.chen28@qq.com>
2020-02-12 13:36:06 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/rewriteClasses.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rewriteClasses.dir/main.cpp.o"
+  "rewriteClasses"
+  "rewriteClasses.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rewriteClasses.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/rewriteClasses.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rewriteClasses.
+# This may be replaced when dependencies are built.
